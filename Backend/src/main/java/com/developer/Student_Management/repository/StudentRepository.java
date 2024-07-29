150a1cb311462504a03be0323fc5d832334d07c3
Mayank Gaur <gaurmayank05@gmail.com>
2024-07-29 23:58:06 +0530

    Update Backend Code

--- Backend/src/main/java/com/developer/Student_Management/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.developer.Student_Management.repository;
-
-import com.developer.Student_Management.entity.StudentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
-}
